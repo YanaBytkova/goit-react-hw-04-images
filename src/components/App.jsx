2f17894b7e66be0c2ae74056f8288a8e7ba480c5
YanaBytkova <nedavnyaya.yana@gmail.com>
2023-11-07 21:19:58 +0200

    fix deploy

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -14,11 +14,11 @@ export const App = () => {
   const [totalHits, setTotalHits] = useState(null);
   const [word, setWord] = useState('');
   const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
+  const [errorMsg, setError] = useState(null);
   const [page, setPage] = useState(1);
   const [modalData, setModalData] = useState(null);
   const [isOpenModal, setIsOpenModal] = useState(false);
-  const [query, setQuery] = useState('');
+  // const [query, setQuery] = useState('');
   
   const onSubmit = (word) => {
     const searchWord = word;
@@ -29,14 +29,12 @@ export const App = () => {
 
   const fetchImages = async () => {
     try {
-      
       setIsLoading(true);
       const { data } = await axios.get(`${API_URL}?q=${word}&page=${page}&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=12`);
       if (images == null) {
           setImages(data.hits);
           setTotalHits(data.totalHits);
       }
-      
       if (page === Math.floor(totalHits / 12)) {
               alert(`...You viewed all images with ${word}! Please, enter new word!`);
             }
@@ -44,31 +42,28 @@ export const App = () => {
             if (images !== null) {
               const addImages = 
                 data.hits;
-                // console.log(this.state.images.length, addImages);
                 setImages(images.concat(addImages));
                 setTotalHits(data.totalHits);
-              // this.setState(prevState => ({
-              //   images: prevState.images.concat(addImages),
-              //   totalHits: data.totalHits,
-              //       }));}
             }
     } catch (error) {
       setError(error.message);
+      console.log(errorMsg);
     } finally {
         setIsLoading(false)};
   }
 
 
   useEffect((prevState) => {
+    
     if (prevState !== word ) {
         setImages(null);
       fetchImages();
     }
-    if (page !== prevState || query !== prevState) {
+    if (page !== prevState) {
       fetchImages();
     }
     
-  }, [word, page, query]);
+  }, [word, page]);
   // componentDidUpdate(_, prevState) {
   //   if (prevState.word !== this.state.word) {
   //     this.setState({
